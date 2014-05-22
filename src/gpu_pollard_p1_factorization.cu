f529bab1ebb865b86a41be087709400b7dd83755
Phil Kwiotek <info@kwiotec.de>
2014-05-22 13:24:49 +0200

    gpu pollard sync abbruchbedingung

--- src/gpu_pollard_p1_factorization.cu
+++ src/gpu_pollard_p1_factorization.cu
@@ -1,11 +1,15 @@
 #include "gpu_pollard_p1_factorization.h"
 #include "gpu_math_stuff.h"
+#include <stdbool.h>
 
 //#define GPU_POLLARD_P1_V2
 #define GPU_POLLARD_P1_V1
 
+__device__ bool *factor_not_found;
+
 void gpu_pollard_p1_factorization(long long int n, long long int* p, long long int* q, unsigned long int *primes, unsigned long int primes_length) {
 	// pointers needed on device
+	bool factor_not_found_host = true;
 	long long int *n_dev, *p_dev;
 	unsigned long int *primes_dev, *primes_length_dev;
 
@@ -14,11 +18,13 @@ void gpu_pollard_p1_factorization(long long int n, long long int* p, long long i
 	cudaMalloc((void **) &p_dev, sizeof(long long int));
 	cudaMalloc((void **) &primes_length_dev, sizeof(unsigned long int));
 	cudaMalloc((void **) &primes_dev, sizeof(unsigned long int) * primes_length);
+	cudaMalloc((void **) &factor_not_found, sizeof(bool));
 	
 	// copy input to device
 	cudaMemcpy(n_dev, &n, sizeof(long long int), cudaMemcpyHostToDevice);
 	cudaMemcpy(primes_length_dev, &primes_length, sizeof(unsigned long int), cudaMemcpyHostToDevice);
 	cudaMemcpy(primes_dev, primes, sizeof(unsigned long int) * primes_length, cudaMemcpyHostToDevice);
+	cudaMemcpy(factor_not_found, &factor_not_found_host, sizeof(bool), cudaMemcpyHostToDevice);
 
 	// calculate a prime factor on gpu
 	gpu_pollard_p1_factor<<<1,1>>>(n_dev, primes_dev, primes_length_dev, p_dev);
@@ -32,6 +38,7 @@ void gpu_pollard_p1_factorization(long long int n, long long int* p, long long i
 	cudaFree(p_dev);
 	cudaFree(primes_length_dev);
 	cudaFree(primes_dev);
+	cudaFree(factor_not_found);
 
 	// calculate other factor on cpu
 	*q = n / *p;
@@ -44,9 +51,10 @@ __global__ void gpu_pollard_p1_factor(long long int *n_in, unsigned long int *pr
 	long long int n = *n_in;
 	unsigned long int primes_length = *primes_length_in;
 
+
 	for (a = 2; a < a_max; a++) {
 
-		for (b = 2; b < b_max; b++) {
+		for (b = 2; b < b_max && *factor_not_found; b++) {
 
 			//calculate e
 #ifdef GPU_POLLARD_P1_V2
@@ -83,6 +91,9 @@ __global__ void gpu_pollard_p1_factor(long long int *n_in, unsigned long int *pr
 				} else {
 					//found a real factor of n
 					*factor_out = g;
+
+					//stop all other threads
+					*factor_not_found = false;
 					return;
 				}
 			}
