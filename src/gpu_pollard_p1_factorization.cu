7e5ebdddec57877243339bb0216917ffe82b1eaf
Jochen Schwander <jochen@yamakasi.org>
2014-05-22 13:54:49 +0200

    gpu pollard parallelisierung die zweite

--- src/gpu_pollard_p1_factorization.cu
+++ src/gpu_pollard_p1_factorization.cu
@@ -10,7 +10,8 @@ __device__ bool *factor_not_found;
 void gpu_pollard_p1_factorization(long long int n, long long int* p, long long int* q, unsigned long int *primes, unsigned long int primes_length) {
 	// pointers needed on device
 	bool factor_not_found_host = true;
-	long long int *n_dev, *p_dev;
+	long long int a, a_max = 1000;
+	long long int *n_dev, *p_dev, *a_dev;
 	unsigned long int *primes_dev, *primes_length_dev;
 
 	// allocate memory on device
@@ -19,6 +20,7 @@ void gpu_pollard_p1_factorization(long long int n, long long int* p, long long i
 	cudaMalloc((void **) &primes_length_dev, sizeof(unsigned long int));
 	cudaMalloc((void **) &primes_dev, sizeof(unsigned long int) * primes_length);
 	cudaMalloc((void **) &factor_not_found, sizeof(bool));
+	cudaMalloc((void **) &a_dev, sizeof(long long int));
 	
 	// copy input to device
 	cudaMemcpy(n_dev, &n, sizeof(long long int), cudaMemcpyHostToDevice);
@@ -26,9 +28,20 @@ void gpu_pollard_p1_factorization(long long int n, long long int* p, long long i
 	cudaMemcpy(primes_dev, primes, sizeof(unsigned long int) * primes_length, cudaMemcpyHostToDevice);
 	cudaMemcpy(factor_not_found, &factor_not_found_host, sizeof(bool), cudaMemcpyHostToDevice);
 
-	// calculate a prime factor on gpu
-	gpu_pollard_p1_factor<<<4,384>>>(n_dev, primes_dev, primes_length_dev, p_dev);
-	cudaDeviceSynchronize();
+	for (a = 2; a < a_max; a++) {
+		// copy a in
+		cudaMemcpy(a_dev, &a, sizeof(long long int), cudaMemcpyHostToDevice);
+
+		// calculate a prime factor on gpu
+		gpu_pollard_p1_factor<<<4,384>>>(n_dev, a_dev, primes_dev, primes_length_dev, p_dev);
+		cudaDeviceSynchronize();
+
+		// check if factor allready found
+		cudaMemcpy(&factor_not_found_host, factor_not_found, sizeof(bool), cudaMemcpyDeviceToHost);
+		if (!factor_not_found_host) {
+			break;
+		}
+	}
 
 	// copy result to host
 	cudaMemcpy(p, p_dev, sizeof(long long int), cudaMemcpyDeviceToHost);
@@ -39,69 +52,63 @@ void gpu_pollard_p1_factorization(long long int n, long long int* p, long long i
 	cudaFree(primes_length_dev);
 	cudaFree(primes_dev);
 	cudaFree(factor_not_found);
+	cudaFree(a_dev);
 
 	// calculate other factor on cpu
 	*q = n / *p;
 }
 
-__global__ void gpu_pollard_p1_factor(long long int *n_in, unsigned long int *primes, unsigned long int *primes_length_in, long long int *factor_out) {
+__global__ void gpu_pollard_p1_factor(long long int *n_in, long long int *a_in, unsigned long int *primes, unsigned long int *primes_length_in, long long int *factor_out) {
 	long long int b_max = 1000000;
-	long long int a_max = 1000;
-	long long int b, e, p, i, a, g;
+	long long int b, e, p, i, g;
 	long long int n = *n_in;
+	long long int a = *a_in;
 	unsigned long int primes_length = *primes_length_in;
-
 
 	long long int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	long long int step_size = blockDim.x*gridDim.x;
 
 
-
-
-	for (a = 2; a < a_max; a++) {
-
-		for (b = 2+idx; b < b_max && *factor_not_found; b+=step_size) {
-
-			//calculate e
+	for (b = 2+idx; b < b_max && *factor_not_found; b+=step_size) {
+		//calculate e
 #ifdef GPU_POLLARD_P1_V2
-			e = 1;
+		e = 1;
 #endif
 #ifdef GPU_POLLARD_P1_V1
-			e = a;
+		e = a;
 #endif
-			for (i = 0; i < primes_length; i++) {
-				p = (long long int) primes[i];
-				if (b >= p) {
+		for (i = 0; i < primes_length; i++) {
+			p = (long long int) primes[i];
+			if (b >= p) {
 #ifdef GPU_POLLARD_P1_V2
-					e *= gpu_power_mod(p, log((long double)b) / log((long double) p), n);
+				e *= gpu_power_mod(p, log((long double)b) / log((long double) p), n);
 #endif
 #ifdef GPU_POLLARD_P1_V1
-					e = gpu_power_mod(e, p, n);
+				e = gpu_power_mod(e, p, n);
 #endif
-				} else {
-					break;
-				}
+			} else {
+				break;
 			}
+		}
 
-			//check if g is a factor of n
+		//check if g is a factor of n
 #ifdef GPU_POLLARD_P1_V2
-			g = gpu_euclidean_gcd(gpu_power_mod(a, e - 1, n), n);
+		g = gpu_euclidean_gcd(gpu_power_mod(a, e - 1, n), n);
 #endif
 #ifdef GPU_POLLARD_P1_V1
-			g = gpu_euclidean_gcd(e - 1, n);
+		g = gpu_euclidean_gcd(e - 1, n);
 #endif
-			if (g > 1) {
-				if (g == n) {
-					//found trivial factor n of n
-					break;
-				} else {
-					//found a real factor of n
-					*factor_out = g;
+		if (g > 1) {
+			if (g == n) {
+				//found trivial factor n of n
+				break;
+			} else {
+				//found a real factor of n
+				*factor_out = g;
 
-					//stop all other threads
-					*factor_not_found = false;
-					return;
-				}
+				//stop all other threads
+				*factor_not_found = false;
+				return;
 			}
 		}
 	}
