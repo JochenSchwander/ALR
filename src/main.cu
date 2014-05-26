07b5796b27423368ffbaa226e88f30a021a66a56
Jochen Schwander <jochen@yamakasi.org>
2014-05-26 10:12:44 +0200

    main aufgeraeumt

--- src/main.cu
+++ src/main.cu
@@ -1,5 +1,4 @@
 #include <stdio.h>
-//#include "factorization.h"
 #include "gpu_pollard_p1_factorization.h"
 #include "pollard_p1_factorization.h"
 #include "gpu_factorization.h"
@@ -11,7 +10,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//#define DEBUG_PRINT_CLOCKS
 
 
 void read_primes(unsigned long int *primes);
@@ -20,18 +18,13 @@ int main() {
 	unsigned long int primes_length = 78498;
 	unsigned long int *primes = (unsigned long int *) malloc(sizeof(unsigned long int) * primes_length);
 
-	//CUDA
-	long long int *dev_n, *dev_p, *dev_q;
 	//time measurement
 	clock_t start, end;
 
-	long long int *p, *host_p, *host_q, *q, *n, e, d;
+	long long int *p, *q, *n, e, d;
 	p = (long long int*)malloc(sizeof(long long int));
 	q = (long long int*)malloc(sizeof(long long int));
 	n = (long long int*)malloc(sizeof(long long int));
-
-	host_p = (long long int*)malloc(sizeof(long long int));
-	host_q = (long long int*)malloc(sizeof(long long int));
 	
 	read_primes(primes);
 
@@ -184,57 +177,6 @@ int main() {
 				break;
 		}
 	}
-
-	/*start = clock();
-	//factorization(*n, p, q);
-	pollard_p1_factorization(*n, p, q, primes, primes_length);
-	end = clock();
-#ifdef DEBUG_PRINT_CLOCKS
-	printf("p = %lld; q = %lld in %lu clocks\n", *p, *q, (unsigned long)(end-start));
-#else
-	printf("p = %lld; q = %lld in %lf seconds\n", *p, *q, (end-start)/(double)CLOCKS_PER_SEC);
-#endif
-	d = calculatePrivateKey(e,*p,*q);
-	printf("d = %lld\n", d);*/
-
-
-	/*//allocate the momory on th GPU
-	cudaMalloc((void **) &dev_n, sizeof(long long int));
-	cudaMalloc((void **) &dev_p, sizeof(long long int));
-	cudaMalloc((void **) &dev_q, sizeof(long long int));
-
-	cudaMemcpy( dev_n, n, sizeof(long long int),cudaMemcpyHostToDevice);
-
-	start = clock();
-	gpu_factorization<<<4,384>>>(dev_n, dev_p, dev_q);
-	cudaDeviceSynchronize();
-	end = clock();
-
-	cudaMemcpy( host_p, dev_p, sizeof(long long int),	cudaMemcpyDeviceToHost);
-	cudaMemcpy( host_q, dev_q, sizeof(long long int),	cudaMemcpyDeviceToHost);
-
-#ifdef DEBUG_PRINT_CLOCKS
-	printf("p = %lld; q = %lld in %lu clocks\n", *host_p, *host_q, (unsigned long)(end-start));
-#else
-	printf("p = %lld; q = %lld in %lf seconds\n", *host_p, *host_q, (end-start)	/(double)CLOCKS_PER_SEC);
-#endif
-
-	cudaFree(dev_p);
-	cudaFree(dev_q);
-	cudaFree(dev_n);
-
-	*/
-
-	/*
-	start = clock();
-	//factorization(*n, p, q);
-	gpu_pollard_p1_factorization(*n, p, q, primes, primes_length);
-	end = clock();
-#ifdef DEBUG_PRINT_CLOCKS
-	printf("p = %lld; q = %lld in %lu clocks\n", *p, *q, (unsigned long)(end-start));
-#else
-	printf("p = %lld; q = %lld in %lf seconds\n", *p, *q, (end-start)/(double)CLOCKS_PER_SEC);
-#endif*/
 
 	system("say das programm wurde erfolgreich ausgefuehrt und martin ist kein kein kein bob! Notiz an Phil /!");
 	return 0;
